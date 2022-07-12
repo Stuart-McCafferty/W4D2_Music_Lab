72d92564dd7e7270b7883c5a7205811d4bdd1171
Stuart-McCafferty <Stuartj678@gmail.com>
2022-07-12 14:42:36 +0100

    initial commit

---
+++ models/album.py
@@ -0,0 +1,9 @@
+class Album:
+    def __init__(self, _ID, _name, _genre, _artist):
+        self.ID = _ID
+        self.name = _name
+        self.genre = _genre
+        self.artist = _artist
+    
+    
+        
